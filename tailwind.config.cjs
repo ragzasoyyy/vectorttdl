ea2e62fa343482124d0dfbfb1e31437d0567a079
ragzasoyyy <ragzasik@gmail.com>
2025-10-20 17:09:15 +0700

    Create tailwind.config.cjs

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}", "./app/**/*.{js,jsx}", "./src/**/*.{js,jsx}", "./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        vdblue: '#0066ff',
+        vdgblue: '#00d4ff'
+      },
+      borderRadius: {
+        xl: '1rem'
+      }
+    }
+  },
+  plugins: []
+};
